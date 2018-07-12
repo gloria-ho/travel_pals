707d63ee2f8c027ba1a22e95f4cce6425b5de2d5
Gloria H <silverdragonia@gmail.com>
2018-07-12 15:39:00 -0400

    code freeze

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # , controllers: {
-    # registrations: 'users/registrations',
-    # checking if user signs up
-    # sessions: 'users/sessions'
-    # checking if user is logged into a session
+  # registrations: 'users/registrations',
+  # checking if user signs up
+  # sessions: 'users/sessions'
+  # checking if user is logged into a session
   # }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
